08982939ac4acadf351b757d7fa2e7392bd50258
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-02-07 20:09:34 +0100

    gjort lite mer på en övningsuppgift

--- js/script.js
+++ js/script.js
@@ -142,6 +142,80 @@
 
 //________________FETCH & AWAIT CHUCK_______________________________________
 
+// const form = document.getElementById('form');
+
+// form.addEventListener('submit', event => {
+//     event.preventDefault();
+
+//     const userSearch = {};
+
+//     const inputElements = form.childNodes;
+//     console.log(inputElements);
+
+//     inputElements.forEach(node => {
+//         if (node.name != null && node.name !== '') {
+//             console.log(node.name, node.value);
+
+//             userSearch[node.name] = node.value;
+//         }
+//     })
+
+
+//     console.log(userSearch);
+
+//     getChuckJoke(userSearch);
+
+// });
+
+
+// async function getChuckJoke({ category }) {
+//     console.log(category);
+
+
+//     // const h1 = document.createElement("h1");
+//     // document.body.append(h1);
+//     // h1.innerText = "Loading";
+//     // h1.style.color = "black";
+
+
+//     const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+
+//     try {
+//         const response = await fetch(url);
+//         console.log(response);
+//         if (response.ok) {
+
+//             const obj = await response.json();
+//             console.log(obj);
+
+//             const {value} = obj;
+//             console.log(value);
+
+//             let divvy = document.createElement('div')
+//             divvy.innerHTML = `${value}`
+//             document.body.appendChild(divvy)
+
+//         }
+//         else {
+//             throw 'Anropet misslyckades';
+//         }
+//     }
+
+
+//     catch (error) {
+//         console.log(error);
+//     }
+
+// }
+
+//_____________FETCH & ASYNC PUNK API______________
+
+// Punk API - https://punkapi.com/documentation/v2 
+// Låt en användare söka på öl genom att ange ett namn. Visa namn, bild och beskrivning för ett enda öl. 
+// Låt en användare ange vilken mat som ska passa till ölen och sök fram öl som passar. Visa namnet för alla öl i svaret.
+// Låt användaren välja ett datum och visa namnet på alla öl som har bryggts innan det datumet. 
+
+
 const form = document.getElementById('form');
 
 form.addEventListener('submit', event => {
@@ -160,57 +234,27 @@ form.addEventListener('submit', event => {
         }
     })
 
-
     console.log(userSearch);
 
-    getChuckJoke(userSearch);
-
+    getBeer(userSearch);
 });
 
-
-async function getChuckJoke({ category }) {
-    console.log(category);
-
-
-    // const h1 = document.createElement("h1");
-    // document.body.append(h1);
-    // h1.innerText = "Loading";
-    // h1.style.color = "black";
+async function getBeer({ beerName }) {
+    console.log(beerName);
 
 
-    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
-
-    try {
-        const response = await fetch(url);
-        console.log(response);
-        if (response.ok) {
-
-            const obj = await response.json();
-            console.log(obj);
-
-            const {value} = obj;
-            console.log(value);
-
-            let divvy = document.createElement('div')
-            divvy.innerHTML = `${value}`
-            document.body.appendChild(divvy)
-
-        }
-        else {
-            throw 'Anropet misslyckades';
-        }
-    }
+    const url = `https://api.punkapi.com/v2/beers/${name}`;
 
 
-    catch (error) {
-        console.log(error);
-    }
+    const response = await fetch(url);
+    console.log(response);
+
+    const obj = await response.json();
+    console.log(obj);
+
+    // const {name} = obj;
+    // console.log(name);
 
 }
 
-//_____________FETCH & ASYNC PUNK API______________
 
-// Punk API - https://punkapi.com/documentation/v2 
-// Låt en användare söka på öl genom att ange ett namn. Visa namn, bild och beskrivning för ett enda öl. 
-// Låt en användare ange vilken mat som ska passa till ölen och sök fram öl som passar. Visa namnet för alla öl i svaret.
-// Låt användaren välja ett datum och visa namnet på alla öl som har bryggts innan det datumet. 
